16eaf64a4050a484fc2506a20e8ba00d8aa4c147
andikaky <andikajelly123@gmail.com>
2025-08-20 15:55:54 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>hallo cok</title>
+        <title>Campaign Is Underway</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
