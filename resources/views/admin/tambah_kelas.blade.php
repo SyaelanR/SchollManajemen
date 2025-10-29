a262e9ffa58e1bdea3a1b6d77e9c73853647a0a4
SyaelanR <nandasyaelan@gmail.com>
2025-10-29 10:09:51 +0700

    fix navbar admin

--- resources/views/admin/tambah_kelas.blade.php
+++ resources/views/admin/tambah_kelas.blade.php
@@ -61,6 +61,10 @@
             </a>
 
             @can('view-admin')
+            <a href="{{ route('admin.profile') }}" class="flex items-center px-6 py-3 text-gray-600 hover:bg-indigo-50 hover:text-indigo-600 transition duration-200 @if(request()->routeIs('admin.profile')) bg-indigo-50 text-indigo-600 font-semibold rounded-r-lg border-l-4 border-indigo-600 @endif">
+                <i class="fa-solid fa-user-circle mr-3"></i>
+                <span>Profil</span>
+            </a>
             <a href="{{ route('manajemenSiswa') }}" class="flex items-center px-6 py-3 text-gray-600 hover:bg-indigo-50 hover:text-indigo-600 transition duration-200">
                 <i class="fa-solid fa-user-graduate w-6 mr-3"></i>
                 <span>Manajemen Siswa</span>
