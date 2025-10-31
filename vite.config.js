5792f630aa73677adf880f36e6b19d65ad7edbc8
SyaelanR <nandasyaelan@gmail.com>
2025-10-31 15:34:51 +0700

    fix confix "vite.config.js"

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,38 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import { resolve } from 'path';
 
 export default defineConfig({
-    // server: {
-    //     host: '0.0.0.0',   // penting: izinkan semua koneksi
-    //     port: 5173,        // optional: ubah jika bentrok
-    //     cors: true,
-    //     hmr: {
-    //         host: '192.168.1.10',
-    //     },
-    // },
+    server: {
+        // host: '0.0.0.0',
+        // port: 5173,
+        // hmr: {
+        //     host: '192.168.1.20',
+        // },
+        watch: {
+            ignored: [
+                resolve(__dirname, 'storage/framework/views/**'),
+            ],
+        }
+    },
+
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            refresh: [
+                'resources/routes/**',
+                'routes/**',
+                'resources/views/**',
+            ],
         }),
     ],
+
+    // KONFIGURASI SERVER SEHARUSNYA DI SINI (LEVEL ATAS)
+    // server: {
+    //     watch: {
+    //         ignored: [
+    //             resolve(__dirname, 'storage/framework/views/**'),
+    //         ],
+    //     }
+    // }
 });
